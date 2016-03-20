da86108b7b86a0665fa39215170881203e454db1
fasteddiegarcia <fasteddiegarcia@gmail.com>
2016-03-20 13:36:22 -0500

    Rendering Collection Views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Bitter</title>
+    <title>Breddit</title>
 
     <!-- Fonts -->
 
@@ -17,8 +17,9 @@
 <body>
     <div id="content"></div>
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.jsR"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
